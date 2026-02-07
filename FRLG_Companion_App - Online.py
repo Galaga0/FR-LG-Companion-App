1ec4b8d8e2c17227f6e153029ea76c4969ad5b48
Galaga0 <799c6y4x84@privaterelay.appleid.com>
2026-02-07 13:10:03 +0100

    Update FRLG_Companion_App - Online.py

--- FRLG_Companion_App - Online.py
+++ FRLG_Companion_App - Online.py
@@ -632,10 +632,10 @@ div[data-testid="stVerticalBlock"]:has(.evo-row-card){
 /* Put the evolve button in the Action area (right side of the row) */
 div[data-testid="stVerticalBlock"]:has(.evo-row-card) div[class*="st-key-evo_btn__"]{
   position: absolute !important;
-  right: 200px !important;
+  right: 150px !important;
 
   /* vertical placement of the button relative to the row */
-  top: 112px !important;            /* <-- adjust this number if needed */
+  top: 300px !important;            /* <-- adjust this number if needed */
   transform: none !important;
 
   z-index: 50 !important;
